c4c12830f1aa1059abf789b198b66a9f1e98f0f6
yashraj-1024 <yashrajpal590@gmail.com>
2025-07-26 19:19:39 +0530

    Time: 4 ms (91.09%) | Memory: 58.6 MB (8.11%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,35 +1,28 @@
 class Solution {
     public int maxArea(int[] height) {
         
-        ArrayList<Integer> heightList = new ArrayList<>();
-        for(int h : height){
-            heightList.add(h);
-        }
-
-        return storeWater(heightList);
-    }
-
-    public static int storeWater(ArrayList<Integer> height){
-
-        int maxwater = 0;
-        int lp =0;
-        int rp = height.size() -1;
+        int lp = 0;
+        int rp = height.length-1;
+        int maxWater = 0;
 
         while(lp < rp){
 
-            int ht = Math.min(height.get(lp),height.get(rp));
-            int width = rp -lp;
+            int ht =  Math.min(height[lp], height[rp]);
+            int width = rp-lp;
 
             int currWater = ht*width;
-            maxwater = Math.max(maxwater,currWater);
 
-            if(height.get(lp) < height.get(rp)){
-                lp++;
+            if(currWater > maxWater){
+                maxWater = currWater;
             }
-            else{
+
+            if(height[lp] < height[rp]){
+                lp++;
+            }else{
                 rp--;
             }
         }
-        return maxwater;
+
+        return maxWater;
     }
 }
